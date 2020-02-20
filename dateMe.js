5a88217e4080ea1bc2d798fe0cb5ace864e3e798
Lisa S <52213257+lisasce@users.noreply.github.com>
2020-02-20 23:55:49 +0100

    finished

--- dateMe.js
+++ dateMe.js
@@ -1,56 +1,80 @@
 var objectPeople;
 objectPeople = [];
 var iArray = [];
+//------------------------------------DATABASE---------------------------------------
+var Gender;
+(function (Gender) {
+    Gender["woman"] = "woman";
+    Gender["man"] = "man";
+    Gender["divers"] = "divers";
+})(Gender || (Gender = {}));
+//Kontrolle über die Werte durch enum.
 var names = ["Kajeen", "Lisa", "Chris", "Oliver", "Mr Y", "Zolfi"];
+var city = ["Heaven", "Hell", "Athen", "Vienna", "Sofia", "Aleppo"];
+var motto = ["Dancing like a queen!", "Pumpkin spice is the pumpkin spice of life...",
+    "#nomakeup #nofilter #selfie #tgif", "Vinyl records an mixtapes only.", "Popped collars are for date nights only ;)", "Beards are like so hot!"];
+var gender = [Gender.woman, Gender.divers, Gender.woman, Gender.man, Gender.man, Gender.divers];
 var age = [21, 33, 23, 25, 22, 29];
 var hobby = ["playing violin", "drawing", "singing", "travelling", "feeding animals", "eating"];
 var img = ["img/frau1.jpg", "img/frau2.jpg", "img/frau3.jpg", "img/mann1.jpg", "img/mann2.jpg", "img/mann3.jpg"];
+//------------------------------------DATABASE END---------------------------------------
 var Person = /** @class */ (function () {
-    //gender: string;
-    function Person(firstName, age, hobby, img) {
+    function Person(firstName, motto, age, hobby, favourite, img, gender, city) {
         this.firstName = firstName;
+        this.motto = motto;
         this.age = age;
         this.hobby = hobby;
-        //this.favourit = favourit;
+        this.favourite = favourite;
         this.img = img;
-        //this.gender = gender;
-        //objectPeople.push(this);
+        this.gender = gender;
+        this.city = city;
     }
     return Person;
 }());
 function creationPerson() {
     for (var i = 0; i < 6; i++) {
-        var profil = new Person(names[i], age[i], hobby[i], img[i]);
+        var profil = new Person(names[i], motto[i], age[i], hobby[i], false, img[i], gender[i], city[i]);
         objectPeople.push(profil);
-        //console.table(profil);
     }
-    ;
 }
 function createCard() {
-    var b = "";
+    var visibleCards = "";
+    var hiddenCards = "";
     for (var i = 0; i < objectPeople.length; i++) {
         iArray.push(i);
-        b += "\n    <div class=\"card mb-3\" >\n        <img src=\"" + objectPeople[i].img + "\" class=\"card-img-top\" alt=\"...\">\n        <div class=\"card-body\">\n            <h5 class=\"card-title\">" + objectPeople[i].firstName + "</h5>\n            <p class=\"card-text\"> " + objectPeople[i].firstName + " is " + objectPeople[i].age + " years old, and loves " + objectPeople[i].hobby + "</p>\n            <a id=\"" + objectPeople[i].firstName + "\" class=\"btn btn-primary\">add to your favorites</a>\n        </div>\n     </div>\n  ";
+        visibleCards += "\n    <div id=\"upperCard" + i + "\" class=\"card mb-3\">\n        <img src=\"" + objectPeople[i].img + "\" class=\"card-img-top\" alt=\"face\">\n        <img id=\"starBtn" + i + "\" src=\"img/star.png\" class=\"star m-1 img-fluid\" alt=\"*\">\n        <div class=\"card-body\">\n            <h5 class=\"card-title\">" + objectPeople[i].firstName + "</h5>\n            <p class=\"card-text\">( " + objectPeople[i].gender + " ) <br> " + objectPeople[i].motto + "</p>\n        </div>\n        <button id=\"Btn" + i + "\" class=\"btn\">add to favorites</button>\n     </div>\n  ";
+        hiddenCards += "\n    <div id=\"favCard" + i + "\" class=\"hiddenCard card mb-3 d-none\">\n        <img src=\"" + objectPeople[i].img + "\" class=\"card-img-top\" alt=\"face\">\n        <img id=\"YstarBtn" + i + "\" src=\"img/starYellow.png\" class=\"star m-1 img-fluid\" alt=\"*\">\n        <div class=\"card-body\">\n            <h5 class=\"card-title\">" + objectPeople[i].firstName + "</h5>\n            <p class=\"card-text\">( " + objectPeople[i].gender + " ) <br> " + objectPeople[i].motto + "</p>\n            <div class=\"detail\">\n                <p>Age: " + objectPeople[i].age + "</p>\n                <p>Hobby: " + objectPeople[i].hobby + "</p>\n                <p>City: " + objectPeople[i].city + "</p>\n                <button>@ me!</button>\n            </div>\n        </div>\n     </div>\n  ";
     }
     ;
-    document.getElementById('cardsDiv').innerHTML = b;
+    document.getElementById('cardsDiv').innerHTML = visibleCards;
+    document.getElementById('favPersons').innerHTML = hiddenCards;
+}
+function clickMe(btn, i) {
+    var fav = document.getElementById("favCard" + i);
+    $(fav).toggleClass("d-none");
+    var star = document.getElementById("starBtn" + i);
+    var starSrc = "img/star.png";
+    if (!objectPeople[i].favourite) {
+        starSrc = "img/starYellow.png";
+    }
+    star.src = starSrc;
+    objectPeople[i].favourite = !objectPeople[i].favourite;
+}
+function setEvent(i) {
+    var starBtn = "starBtn" + i;
+    var Btn = "Btn" + i;
+    var YstarBtn = "YstarBtn" + i;
+    document.getElementById(starBtn).addEventListener("click", function () {
+        clickMe(this, i);
+    });
+    document.getElementById(Btn).addEventListener("click", function () {
+        clickMe(this, i);
+    });
+    document.getElementById(YstarBtn).addEventListener("click", function () {
+        clickMe(this, i);
+    });
+    $("favCard" + i).toggle();
 }
 creationPerson();
-console.table(objectPeople);
 createCard();
-// function clickMe(i,id){
-//   let id = ${objectPeople[i].firstName};
-//     b= `
-//     <div class="card mb-3" >
-//         <img src="${objectPeople[i].img}" class="card-img-top" alt="...">
-//         <div class="card-body">
-//             <h5 class="card-title">${objectPeople[i].firstName}</h5>
-//             <p class="card-text"> ${objectPeople[i].firstName} is ${objectPeople[i].age} years old, and loves ${objectPeople[i].hobby}</p>
-//             <a id="${objectPeople[i].firstName}" class="btn btn-primary">add to your favorites</a>
-//         </div>
-//      </div>
-//   `;
-//   document.getElementById(id).addEventListener("click", function(){
-//     document.getElementById("favouritPersons").innerHTML = b;
-//   })
-// }
+iArray.forEach(function (i) { setEvent(i); });
--- dateMe.ts
+++ dateMe.ts
@@ -1,84 +1,123 @@
 let objectPeople: Array<Person>;
 objectPeople = [];
-
 let iArray = [];
 
+//------------------------------------DATABASE---------------------------------------
+enum Gender{
+    woman= "woman",
+    man = "man",
+    divers = "divers"
+}
+//Kontrolle über die Werte durch enum.
 let names: Array<string>= ["Kajeen","Lisa","Chris","Oliver","Mr Y","Zolfi"];
+let city: Array<string>= ["Heaven","Hell","Athen","Vienna","Sofia","Aleppo"];
+let motto: Array<string> = ["Dancing like a queen!", "Pumpkin spice is the pumpkin spice of life...",
+"#nomakeup #nofilter #selfie #tgif", "Vinyl records an mixtapes only.", "Popped collars are for date nights only ;)", "Beards are like so hot!"];
+let gender: Array<Gender>= [Gender.woman, Gender.divers, Gender.woman, Gender.man, Gender.man, Gender.divers];
 let age: Array<number>= [21,33,23,25,22,29];
 let hobby: Array<string>= ["playing violin","drawing","singing","travelling","feeding animals","eating"];
 let img: Array<string>= ["img/frau1.jpg","img/frau2.jpg","img/frau3.jpg","img/mann1.jpg","img/mann2.jpg","img/mann3.jpg"];
 
+//------------------------------------DATABASE END---------------------------------------
 
+class Person {
+    firstName: string;
+    motto: string;
+    age: number;
+    hobby: string;
+    favourite: boolean;
+    img: string;
+    gender: Gender;
+    city: string;
 
-class Person { 
-   firstName: string;
-   age: number;
-   hobby: string;
-   //favourit: boolean;
-   img: string;
-   //gender: string;
-
-    constructor(firstName, age, hobby, img) {
-       this.firstName = firstName;
-       this.age = age;
-       this.hobby = hobby;
-       //this.favourit = favourit;
-       this.img = img;
-       //this.gender = gender;
-       //objectPeople.push(this);
-
-   }
-// 
- }
+    constructor(firstName, motto, age, hobby,favourite, img, gender, city) {
+        this.firstName = firstName;
+        this.motto = motto;
+        this.age = age;
+        this.hobby = hobby;
+        this.favourite = favourite;
+        this.img = img;
+        this.gender = gender;
+        this.city = city;
+    }
+}
 
 function creationPerson(){
- for (let i = 0; i<6; i++){
-   let profil = new Person( names[i], age[i], hobby[i], img[i])
-   objectPeople.push(profil);
-   //console.table(profil);
- };
-
+    for (let i = 0; i<6; i++){
+        let profil = new Person( names[i], motto[i], age[i], hobby[i], false, img[i], gender[i], city[i])
+        objectPeople.push(profil);
+    }
 }
 
 function createCard(){
-  var b = "";
-  for (let i=0; i < objectPeople.length; i++) {
-   iArray.push(i);
-  b += `
-    <div class="card mb-3" >
-        <img src="${objectPeople[i].img}" class="card-img-top" alt="...">
+    var visibleCards = "";
+    var hiddenCards = "";
+    for (let i=0; i < objectPeople.length; i++) {
+        iArray.push(i);
+        visibleCards += `
+    <div id="upperCard${i}" class="card mb-3">
+        <img src="${objectPeople[i].img}" class="card-img-top" alt="face">
+        <img id="starBtn${i}" src="img/star.png" class="star m-1 img-fluid" alt="*">
         <div class="card-body">
             <h5 class="card-title">${objectPeople[i].firstName}</h5>
-            <p class="card-text"> ${objectPeople[i].firstName} is ${objectPeople[i].age} years old, and loves ${objectPeople[i].hobby}</p>
-            <a id="${objectPeople[i].firstName}" class="btn btn-primary">add to your favorites</a>
+            <p class="card-text">( ${objectPeople[i].gender} ) <br> ${objectPeople[i].motto}</p>
+        </div>
+        <button id="Btn${i}" class="btn">add to favorites</button>
+     </div>
+  `;
+
+        hiddenCards += `
+    <div id="favCard${i}" class="hiddenCard card mb-3 d-none">
+        <img src="${objectPeople[i].img}" class="card-img-top" alt="face">
+        <img id="YstarBtn${i}" src="img/starYellow.png" class="star m-1 img-fluid" alt="*">
+        <div class="card-body">
+            <h5 class="card-title">${objectPeople[i].firstName}</h5>
+            <p class="card-text">( ${objectPeople[i].gender} ) <br> ${objectPeople[i].motto}</p>
+            <div class="detail">
+                <p>Age: ${objectPeople[i].age}</p>
+                <p>Hobby: ${objectPeople[i].hobby}</p>
+                <p>City: ${objectPeople[i].city}</p>
+                <button>@ me!</button>
+            </div>
         </div>
      </div>
   `;
-      
-  };  
-document.getElementById('cardsDiv').innerHTML = b;
+    };
+    document.getElementById('cardsDiv').innerHTML = visibleCards;
+    document.getElementById('favPersons').innerHTML = hiddenCards;
+}
+
+function clickMe(btn, i){
+    let fav = document.getElementById("favCard"+i);
+    $(fav).toggleClass("d-none");
+    
+    let star = document.getElementById("starBtn" + i);
+    let starSrc = "img/star.png";
+    if (!objectPeople[i].favourite){
+        starSrc = "img/starYellow.png";
+    }
+    star.src=starSrc;
+    objectPeople[i].favourite = !objectPeople[i].favourite;
+}
+
+function setEvent(i) {
+    let starBtn = "starBtn" + i;
+    let Btn = "Btn" + i;
+    let YstarBtn = "YstarBtn" + i;
+
+    document.getElementById(starBtn).addEventListener("click", function () {
+        clickMe(this, i);
+    });
+    document.getElementById(Btn).addEventListener("click", function () {
+        clickMe(this, i);
+    });
+    document.getElementById(YstarBtn).addEventListener("click", function () {
+        clickMe(this, i);
+    });
+    $("favCard" + i).toggle();
 }
 
 
-
 creationPerson();
-console.table(objectPeople);
 createCard();
-
-// function clickMe(i,id){
-//   let id = ${objectPeople[i].firstName};
-//     b= `
-//     <div class="card mb-3" >
-//         <img src="${objectPeople[i].img}" class="card-img-top" alt="...">
-//         <div class="card-body">
-//             <h5 class="card-title">${objectPeople[i].firstName}</h5>
-//             <p class="card-text"> ${objectPeople[i].firstName} is ${objectPeople[i].age} years old, and loves ${objectPeople[i].hobby}</p>
-//             <a id="${objectPeople[i].firstName}" class="btn btn-primary">add to your favorites</a>
-//         </div>
-//      </div>
-//   `;
-
-//   document.getElementById(id).addEventListener("click", function(){
-//     document.getElementById("favouritPersons").innerHTML = b;
-//   })
-// }
+iArray.forEach((i)=>{setEvent(i)});
